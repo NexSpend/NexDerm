89bfc36021b8dc618ab46bc43d2d3aa572488ddc
tanvir313 <singht48@mcmaster.ca>
2026-03-23 17:39:27 -0400

    Added 2 new doctor portal pages.

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.api_v1.endpoints.general_routes import report_route
+from app.api.api_v1.endpoints import doctor_routes # Import the new doctor_routes
 
 api_router = APIRouter()
 
@@ -19,3 +20,4 @@ api_router.include_router(ping.router, prefix="/ping", tags=["ping"])
 # api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(predictions.router, prefix="/predictions", tags=["predictions"])
 api_router.include_router(report_route.router)
+api_router.include_router(doctor_routes.router, prefix="/doctors", tags=["doctors"]) # Include the new doctor_routes
